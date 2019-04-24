e5ab109f6f11cf908ce7f2fc5990ccc59c006ee0
Carl Chadziutko <carl.chadz@gmail.com>
2019-04-24 15:03:30 -0400

    Fixed para-vagrant.sh

--- Scripts/para-vagrant.sh
+++ Scripts/para-vagrant.sh
@@ -1,12 +1,55 @@
---2019-04-24 12:02:25--  https://github.com/joemiller/sensu-tests/blob/master/para-vagrant.sh
-Resolving github.com (github.com)... 192.30.253.113, 192.30.253.112
-Connecting to github.com (github.com)|192.30.253.113|:443... connected.
-HTTP request sent, awaiting response... 200 OK
-Length: unspecified [text/html]
-Saving to: ‘para-vagrant.sh.1’
+#!/bin/sh
 
-     0K .......... .......... .......... .......... ..........  782K
-    50K .......... .......... .                                2.54M=0.07s
+# concurrency is hard, let's have a beer
 
-2019-04-24 12:02:26 (988 KB/s) - ‘para-vagrant.sh.1’ saved [72976]
+# any valid parallel argument will work here, such as -P x.
+MAX_PROCS="--use-cpus-instead-of-cores"
 
+parallel_provision() {
+    while read box; do
+        echo $box
+     done | parallel $MAX_PROCS -I"BOX" -q \
+        sh -c 'LOGFILE="logs/BOX.out.txt" ;                                 \
+                printf  "[BOX] Provisioning. Log: $LOGFILE, Result: " ;     \
+                vagrant provision BOX >$LOGFILE 2>&1 ;                      \
+                RETVAL=$? ;                                                 \
+                if [ $RETVAL -gt 0 ]; then                                  \
+                    echo " FAILURE";                                        \
+                    tail -12 $LOGFILE | sed -e "s/^/[BOX]  /g";             \
+                    echo "[BOX] ---------------------------------------------------------------------------";   \
+                    echo "FAILURE ec=$RETVAL" >>$LOGFILE;                   \
+                else                                                        \
+                    echo " SUCCESS";                                        \
+                    tail -5 $LOGFILE | sed -e "s/^/[BOX]  /g";              \
+                    echo "[BOX] ---------------------------------------------------------------------------";   \
+                    echo "SUCCESS" >>$LOGFILE;                              \
+                fi;                                                         \
+                exit $RETVAL'
+
+    failures=$(egrep  '^FAILURE' logs/*.out.txt | sed -e 's/^logs\///' -e 's/\.out\.txt:.*//' -e 's/^/  /')
+    successes=$(egrep '^SUCCESS' logs/*.out.txt | sed -e 's/^logs\///' -e 's/\.out\.txt:.*//' -e 's/^/  /')
+
+    echo
+    echo "Failures:"
+    echo '------------------'
+    echo "$failures"
+    echo
+    echo "Successes:"
+    echo '------------------'
+    echo "$successes"
+}
+
+## -- main -- ##
+
+# cleanup old logs
+mkdir logs >/dev/null 2>&1 
+rm -f logs/*.out.txt
+
+# start boxes sequentially to avoid vbox explosions
+echo ' ==> Calling "vagrant up" to boot the boxes...'
+vagrant up --no-provision
+
+# but run provision tasks in parallel
+echo " ==> Beginning parallel 'vagrant provision' processes ..."
+
+ruby -rjson -e 'puts JSON.parse(ARGF.read).keys' boxes.json | parallel_provision
