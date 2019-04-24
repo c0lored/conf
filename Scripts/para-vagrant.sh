48f33060e1b6bc437b5f077f5e3282694c1159a1
Carl Chadziutko <carl.chadz@gmail.com>
2019-04-24 12:04:11 -0400

    Added para-vagrant

---
+++ Scripts/para-vagrant.sh
@@ -0,0 +1,12 @@
+--2019-04-24 12:02:25--  https://github.com/joemiller/sensu-tests/blob/master/para-vagrant.sh
+Resolving github.com (github.com)... 192.30.253.113, 192.30.253.112
+Connecting to github.com (github.com)|192.30.253.113|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: unspecified [text/html]
+Saving to: ‘para-vagrant.sh.1’
+
+     0K .......... .......... .......... .......... ..........  782K
+    50K .......... .......... .                                2.54M=0.07s
+
+2019-04-24 12:02:26 (988 KB/s) - ‘para-vagrant.sh.1’ saved [72976]
+
